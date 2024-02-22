2de080cecb772c802cd29873aed98b2ba558e6ea
LaptopPC <shibapediaz4@gmail.com>
2024-02-22 21:37:53 +0900

    Initial commit

---
+++ src/app/components/CalcButtons.tsx
@@ -0,0 +1,9 @@
+interface Props {
+    targetValue1: number;
+    targetValue2: number;
+    answer: number;
+}
+
+export default function CalcButtons({targetValue1, targetValue2, answer} : Props) {
+    
+}
